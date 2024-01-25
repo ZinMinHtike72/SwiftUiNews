e4fda336316b78e6ca81a7ddfa32566538049bac
ZMH <zmh1.sabahna@gmail.com>
2024-01-25 08:54:55 +0630

    Initial Commit

---
+++ SwiftUiNews/SwiftUiNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiNewsApp.swift
+//  SwiftUiNews
+//
+//  Created by ZMH Sabahna on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
